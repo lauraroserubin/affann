845e6de453da05c400a1303958de1afa177f163d
lauraroserubin <lauraroserubin@gmail.com>
2024-11-22 15:42:44 +0530

    Update oct 19 2.py

--- oct 19 2.py
+++ oct 19 2.py
@@ -6,3 +6,10 @@ last_name_length=len(first_name)
 print(last_name_length)
 extracted_last_name=name[last_name_length+1:]
 print(extracted_last_name)
+
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(1, 2 * i):
+        print("*", end="")
+    print()
